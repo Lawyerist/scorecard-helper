c11e6727ad9c6e74f1cfa8197e5def5269d3be16
Sam Glover <sam@samglover.net>
2020-02-21 09:07:28 -0600

    Update scorecard-helper.php

--- scorecard-helper.php
+++ scorecard-helper.php
@@ -11,17 +11,20 @@ Author URI: http://lawyerist.com
 
 if ( !defined( 'ABSPATH' ) ) exit;
 
-
-
 require_once( plugin_dir_path( __FILE__ ) . 'common/scorecard-helper-common.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'frontend/scorecard-helper-frontend.php' );
 
 if ( is_admin() ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'admin/scorecard-helper-admin.php' );
 }
 
-function scorecard_helper_frontend_stylesheet() {
-	wp_enqueue_style( 'scorecard-helper-frontend-css', plugins_url( 'frontend/scorecard-helper-frontend.css', __FILE__ ) );
-}
+if ( !is_admin() ) {
 
-add_action( 'wp_enqueue_scripts', 'scorecard_helper_frontend_stylesheet' );
+	require_once( plugin_dir_path( __FILE__ ) . 'frontend/scorecard-helper-frontend.php' );
+
+	function scorecard_helper_frontend_stylesheet() {
+		wp_enqueue_style( 'scorecard-helper-frontend-css', plugins_url( 'frontend/scorecard-helper-frontend.css', __FILE__ ) );
+	}
+
+	add_action( 'wp_enqueue_scripts', 'scorecard_helper_frontend_stylesheet' );
+
+}
